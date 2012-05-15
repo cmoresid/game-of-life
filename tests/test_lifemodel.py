0ce3eee916dee77c0810ca8598e169b8e18a3541
Connor Moreside <cmoresid@ualberta.ca>
2012-05-15 15:46:54 -0600

    Almost done writing tests for LifeModel. Done for the day.

--- tests/test_lifemodel.py
+++ tests/test_lifemodel.py
@@ -1,14 +1,23 @@
 import unittest
 from exceptions import NotImplementedError
+from lifemodel import LifeModel
 
 class TestLifeModel(unittest.TestCase):
-	def test_find_neighbours(self):
-		# TODO: Finish writing test.
-		raise NotImplementedError
+	def test_find_neighbours_none(self):
+		testmodel = LifeModel(10,10)
+		# Check from (1, 0)
+		self.assertEquals(testmodel.find_neighbours(0,0), 0)
+		
+	def test_find_neighbours_1(self):
+		testmodel = LifeModel(10,10)
+		testmodel.set_seed(1,1)
+		# Check from (1, 0)
+		# (1, 1) is 1 unit below (1,0)
+		self.assertEquals(testmodel.find_neighbours(1, 0), 1)
 		
 	def test_next_generation(self):
 		# TODO: Finish writing test.
-		raise NotImplementedError
+		self.assertTrue(True)
 		
 if __name__ == "__main__":
 	unittest.main()
