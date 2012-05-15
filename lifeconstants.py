25135dd2bb8d7b660c10b9905eb8e5f900f6f74a
Connor Moreside <cmoresid@ualberta.ca>
2012-05-15 08:59:36 -0600

    Integrated constants into lifegame.py. Refactored events into their own separate methods. Cleaned up the code base a bit.

--- lifeconstants.py
+++ lifeconstants.py
@@ -1,20 +1,19 @@
 # Color constants
-black = (0,0,0)
-white = (255,255,255)
-green = (0,255,0)
-grey = (192,192,192)
-
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+GREY = (192,192,192)
 # Visible view is 25
 # + 2 since the toolbar hides two cells
-DEFAULT_BOARD_SIZE = 25 + 2
+BOARD_SIZE = 25 + 2
 # Size of each cell on the board
 CELL_SIZE = 25
 # Size of the margin surrounding cells
 MARGIN = 1
 # Screen size
 SCREEN_SIZE = [ \
-	(CELL_SIZE*DEFAULT_BOARD_SIZE)+DEFAULT_BOARD_SIZE, \
-	(CELL_SIZE*DEFAULT_BOARD_SIZE)+DEFAULT_BOARD_SIZE
+	(CELL_SIZE*BOARD_SIZE)+BOARD_SIZE, \
+	(CELL_SIZE*BOARD_SIZE)+BOARD_SIZE
 ]
 # The height of the toolbar
 TOOLBAR_HEIGHT = 50 + 2
@@ -28,7 +27,9 @@ CLEAR_BUTTON = "img/clear_button.png"
 # Default frame rate
 FRAME_RATE = 12
 # Locations of various components on screen
-GENERATION_LABEL_LOC = (SCREEN_SIZE[0]-150, 1)
-GENERATION_LOC 		 = (SCREEN_SIZE[0]-50, 16)
+GENERATION_LABEL_LOC = (SCREEN_SIZE[0]-200, 8)
+GENERATION_LOC 		 = (SCREEN_SIZE[0]-70, 8)
 START_BUTTON_LOC     = (20, 0)
 CLEAR_BUTTON_LOC     = (130, 0)
+# Font size
+FONT_SIZE = 22
--- lifegame.py
+++ lifegame.py
@@ -2,99 +2,90 @@ import pygame
 from lifemodel import LifeModel
 from lifeconstants import *
 
-black = (0,0,0)
-white = (255,255,255)
-green = (0,255,0)
-grey = (192,192,192)
-
 class GameOfLife:
 	def __init__(self):
 		pygame.init()		
-		self.cell_size = 25
-		self.width  = 25 + 2
-		self.height = 25 + 2
-		self.margin = 1
-		self.screen_size = [ \
-			self.cell_size*self.width+(self.width), \
-			self.cell_size*self.height+(self.width) \
-		]
 
-		self.toolbar_height = 50 + 2
-		self.toolbar = pygame.surface.Surface((self.screen_size[0], self.toolbar_height))
-			
-		self.model = LifeModel(self.width, self.height)
-
-		self.screen = pygame.display.set_mode(self.screen_size)
-		pygame.display.set_caption("Game of Life")		
+		self.toolbar = pygame.surface.Surface((SCREEN_SIZE[0], TOOLBAR_HEIGHT))
+		self.screen = pygame.display.set_mode(SCREEN_SIZE)
+		pygame.display.set_caption(WINDOW_CAPTION)	
 		
-		self.game_text = pygame.font.SysFont("Comic Sans MS", 24)
-		self.generation_label = self.game_text.render("Generation:", 1, black)
+		self.model = LifeModel(BOARD_SIZE, BOARD_SIZE)
+		self.start = False
+		self.done  = False
 
-		self.start_button = pygame.image.load("img/start_button.png").convert()
-		self.clear_button = pygame.image.load("img/clear_button.png").convert()
+		self.game_text = pygame.font.SysFont("Comic Sans MS", FONT_SIZE)
+		self.generation_label = self.game_text.render(GENERATION_TEXT, 1, BLACK)
+
+		self.start_button = pygame.image.load(START_BUTTON).convert()
+		self.clear_button = pygame.image.load(CLEAR_BUTTON).convert()
 
 		self.clock = pygame.time.Clock()
-		self.speed = 12
+		self.speed = FRAME_RATE
 	
-	def game_loop(self):
-		done = False
-		start = False
-		
-		while done == False:
+	def game_loop(self):		
+		while self.done == False:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
-					done = True
+					self.done = True
 				if event.type == pygame.MOUSEBUTTONDOWN:
 					pos = pygame.mouse.get_pos()
 				
-					if self.start_button.get_rect().move(20,0).collidepoint(pos):
-						start = False if start else True
-
-					if self.clear_button.get_rect().move(130,0).collidepoint(pos):
-						start = False
-						self.model.clear_world()	
-
-					y = pos[0] // (self.cell_size+self.margin)
-					x = pos[1] // (self.cell_size+self.margin)
-
-					self.model.set_seed(x, y)
+					self.detect_button_events(pos)
+					self.set_cell_state(pos)
 			
-			if start:
+			if self.start:
 				self.model.next_generation()
 			
-			self.screen.fill(black)
+			self.screen.fill(BLACK)
 
-			self.screen.set_clip(0,0,self.screen_size[0], self.toolbar_height)
+			self.screen.set_clip(0,0,SCREEN_SIZE[0], TOOLBAR_HEIGHT)
 			self.draw_toolbar()			
 
-			self.screen.set_clip(0,self.toolbar_height,self.screen_size[0], self.screen_size[1])			
+			self.screen.set_clip(0,TOOLBAR_HEIGHT,SCREEN_SIZE[0], SCREEN_SIZE[1])			
 			self.draw_world()
 
-			self.clock.tick(self.speed)
+			self.clock.tick(FRAME_RATE)
 			pygame.display.flip()
+	
+	def detect_button_events(self, pos):
+		if self.start_button.get_rect().move(START_BUTTON_LOC).collidepoint(pos):
+			self.start = False if self.start else True
 
+		if self.clear_button.get_rect().move(CLEAR_BUTTON_LOC).collidepoint(pos):
+			self.start = False
+			self.model.clear_world()
+			
+	def set_cell_state(self, pos):
+		y = pos[0] // (CELL_SIZE+MARGIN)
+		x = pos[1] // (CELL_SIZE+MARGIN)
+
+		self.model.set_seed(x, y)
+	
 	def draw_toolbar(self):
-		self.toolbar.fill(grey)
+		self.toolbar.fill(GREY)
 		self.screen.blit(self.toolbar, (0,0))	
-		self.screen.blit(self.generation_label, (self.screen_size[0]-150,16))
-		gen = self.game_text.render(str(self.model.generation), 1, black)
-		self.screen.blit(gen, (self.screen_size[0]-50,16))
-		self.screen.blit(self.start_button, (20, 0))
-		self.screen.blit(self.clear_button, (130, 0))
+		
+		self.screen.blit(self.generation_label, GENERATION_LABEL_LOC)
+		
+		gen = self.game_text.render(str(self.model.generation), 1, BLACK)
+		self.screen.blit(gen, GENERATION_LOC)
+		self.screen.blit(self.start_button, START_BUTTON_LOC)
+		self.screen.blit(self.clear_button, CLEAR_BUTTON_LOC)
 
 	def draw_world(self):
-		for x in range(self.width):
-			for y in range(self.height):
+		for x in range(BOARD_SIZE):
+			for y in range(BOARD_SIZE):
 				if self.model.get_currentgrid_xy(x, y) == False:
-					color = white
+					color = WHITE
 				else:
-					color = green
+					color = GREEN
 
 				pygame.draw.rect(self.screen, color, [ \
-					self.margin+(self.cell_size+self.margin)* y, \
-					self.margin+(self.cell_size+self.margin)* x, \
-					self.cell_size, \
-					self.cell_size \
+					MARGIN+(CELL_SIZE+MARGIN)* y, \
+					MARGIN+(CELL_SIZE+MARGIN)* x, \
+					CELL_SIZE, \
+					CELL_SIZE \
 				])
 	
 	def quit(self):
