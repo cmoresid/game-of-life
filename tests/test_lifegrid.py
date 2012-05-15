c765376579ebca56fb4bec2627eb3b5a341f11aa
Connor Moreside <cmoresid@ualberta.ca>
2012-05-15 14:56:57 -0600

    Added tests/. Need to finish writing them now.

---
+++ tests/test_lifegrid.py
@@ -0,0 +1,10 @@
+import unittest
+from exceptions import NotImplementedError
+
+class TestLifeGrid(unittest.TestCase):
+	def test_peek(self):
+		# TODO: Finish writing test.
+		raise NotImplementedError
+		
+if __name__ == "__main__":
+	unittest.main()
---
+++ tests/test_lifemodel.py
@@ -0,0 +1,14 @@
+import unittest
+from exceptions import NotImplementedError
+
+class TestLifeModel(unittest.TestCase):
+	def test_find_neighbours(self):
+		# TODO: Finish writing test.
+		raise NotImplementedError
+		
+	def test_next_generation(self):
+		# TODO: Finish writing test.
+		raise NotImplementedError
+		
+if __name__ == "__main__":
+	unittest.main()
